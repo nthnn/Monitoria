b109cd32688eeaf5a4b6cde3b0345e956059e452
nthnn <nathanneisip@gmail.com>
2023-06-23 23:30:57 +0800

    Successful migration to ESP8266 NodeMCU board.

--- Monitoria-Attendance-Web-App/scripts/index.js
+++ Monitoria-Attendance-Web-App/scripts/index.js
@@ -1,10 +1,8 @@
 const path = require("path");
 const $ = require("jquery");
-const SerialPort = require("serialport").SerialPort;
 const sqlite3 = require("sqlite3").verbose();
 const md5 = require("md5");
 const base64 = require("base-64");
-const { ReadlineParser } = require("@serialport/parser-readline");
 const jQuery = $;
 const JsBarcode = require("jsbarcode");
 
@@ -14,18 +12,15 @@ window.jQuery = window.$ = $;
 $.DataTable = require("datatables.net-bs5")(window, $);
 
 let runtime = {
-    port: null,
-    serialPort: null,
-    serialPortPipe: null,
+    server: "http://192.168.1.1:80",
     interval: null,
     moveToSectionEvent: null,
     db: new sqlite3.Database(path.resolve(__dirname, "./db/main_db.db"))
 };
 
 let cache = {
-    page: null,
-    userId: null,
-    prevPorts: []
+    page: "logs",
+    userId: null
 }
 
 const Modal = {
@@ -63,7 +58,6 @@ const App = {
 
             $("#" + cache.page + "-nav").removeClass("active");
             $("#" + section + "-nav").addClass("active");
-
             cache.page = section;
 
             if(runtime.moveToSectionEvent) {
@@ -77,15 +71,7 @@ const App = {
     addEntityModal: ()=> {
         Modal.showModal("add-entity");
 
-        runtime.serialPort = new SerialPort({
-            path: runtime.port,
-            baudRate: 9600
-        });
-
-        runtime.moveToSectionEvent = ()=> runtime.serialPort.close();
-        runtime.serialPortPipe = runtime.serialPort.pipe(new ReadlineParser());
-
-        runtime.serialPortPipe.on("data", (data)=> {
+        $.post(runtime.server + "/read", {}, (data)=> {
             let rfid = data.toString().trim();
 
             $("#add-entity-no-rfid").addClass("d-none");
@@ -109,37 +95,15 @@ const App = {
         }, 3000);
     },
 
-    loginSection: ()=> {
-        runtime.interval = setInterval(()=> {
-            let availablePorts = $("#available-ports");
-
-            SerialPort.list()
-                .then((ports) => {
-                    if(JSON.stringify(cache.prevPorts) === JSON.stringify(ports))
-                        return;
-
-                    availablePorts.html("");
-                    cache.prevPorts = ports;
-
-                    if(ports.length == 0)
-                        availablePorts.append("<option selected disabled>No available port.</option>");
-                    else ports.forEach((port)=>
-                        availablePorts.append("<option value=\"" + port.path + "\">" + port.friendlyName + "</option>"));
-                })
-                .catch((err) => availablePorts.append("<option disabled>Cannot read serial ports.</option>"));
-        }, 1000);
-    },
+    loginSection: ()=> { },
 
     logout: ()=> {
-        if(runtime.serialPort.isOpen())
-            runtime.serialPort.close();
         $("#main-content").addClass("animate__slideOutUp");
-
         setTimeout(()=> window.location.reload(), 1000);
     },
 
     processLogin: ()=> {
-        let username = $("#username").val(), password = $("#password").val(), port = $("#available-ports option:selected").val();
+        let username = $("#username").val(), password = $("#password").val();
 
         $("#login-error-message").addClass("d-none");
         if(!username || username == "") {
@@ -163,46 +127,47 @@ const App = {
             return;
         }
 
-        if(!port || port == "No available port.") {
+        $.post(runtime.server + "/check", {}, (data)=> {
+            if(data == "OK") {
+                username = base64.encode(username);
+                password = base64.encode(md5(password));
+
+                runtime.db.serialize(() => {
+                    runtime.db.all("SELECT id, username, account_type, password FROM admins WHERE username=\"" + username + "\" AND password=\"" + password + "\"", (err, rows) => {
+                        if(err) {
+                            $("#login-error-message").removeClass("d-none");
+                            $("#login-error-text").html("Something went wrong.");
+                        }
+                        else {
+                            if(rows.length >= 1 && password == rows[0].password) {
+                                $("#login-section").removeClass("animate__slideInDown").addClass("animate__slideOutUp");
+
+                                setTimeout(()=> $("#login-section").removeClass("d-flex").addClass("d-none"), 1000);
+                                setTimeout(()=> {
+                                    $("#logs-section").removeClass("d-none").addClass("animate__slideInDown");
+                                    $("#main-navbar").removeClass("d-none").addClass("animate__slideInDown");
+
+                                    setTimeout(()=> $("#main-navbar").addClass("d-block"), 1000);
+                                    clearInterval(runtime.interval);
+
+                                    App.showLogs();
+                                }, 1500);
+                            }
+                            else {
+                                $("#login-error-message").removeClass("d-none");
+                                $("#login-error-text").html("Invalid username or password.");
+                            }
+                        }
+                    });
+                });
+            }
+            else {
+                $("#login-error-message").removeClass("d-none");
+                $("#login-error-text").html("No open Monitoria server.");
+            }
+        }).fail(()=> {
             $("#login-error-message").removeClass("d-none");
-            $("#login-error-text").html("No selected port.");
-
-            return;
-        }
-
-        username = base64.encode(username);
-        password = base64.encode(md5(password));
-
-        runtime.db.serialize(() => {
-            runtime.db.all("SELECT id, username, account_type, password FROM admins WHERE username=\"" + username + "\" AND password=\"" + password + "\"", (err, rows) => {
-                if(err) {
-                    $("#login-error-message").removeClass("d-none");
-                    $("#login-error-text").html("Something went wrong.");
-                }
-                else {
-                    if(rows.length >= 1 && password == rows[0].password) {
-                        $("#login-section").removeClass("animate__slideInDown").addClass("animate__slideOutUp");
-
-                        setTimeout(()=> $("#login-section").removeClass("d-flex").addClass("d-none"), 1000);
-                        setTimeout(()=> {
-                            $("#logs-section").removeClass("d-none").addClass("animate__slideInDown");
-                            $("#main-navbar").removeClass("d-none").addClass("animate__slideInDown");
-
-                            setTimeout(()=> $("#main-navbar").addClass("d-block"), 1000);
-                            clearInterval(runtime.interval);
-
-                            cache.page = "logs";
-                            runtime.port = port;
-
-                            App.showLogs();
-                        }, 1500);
-                    }
-                    else {
-                        $("#login-error-message").removeClass("d-none");
-                        $("#login-error-text").html("Invalid username or password.");
-                    }
-                }
-            });
+            $("#login-error-text").html("No open Monitoria server.");
         });
     },
 
@@ -215,30 +180,18 @@ const App = {
 
                     for(let attendee_row of attendee_rows)
                         $("#log-table").prepend("<tr><td>" + attendee_row.date_time + "</td><td>" + attendee_row.ent_id + "</td><td>" + attendee_row.phone_number + "</td><td>" + attendee_row.name + "</td></tr>");
-                        dataTable = $('#logs').DataTable({
-                            ordering: false
-                        });
+                        dataTable = $('#logs').DataTable({ ordering: false });
                 }
 
-                runtime.serialPort = new SerialPort({
-                    path: runtime.port,
-                    baudRate: 9600
-                });
-
-                runtime.moveToSectionEvent = ()=> {
-                    runtime.serialPort.close();
-                    dataTable.destroy();
-
-                    runtime.db.close();
-                    runtime.db = new sqlite3.Database(path.resolve(__dirname, "./db/main_db.db"));
-                };
-
-                runtime.serialPortPipe = runtime.serialPort.pipe(new ReadlineParser());
-                runtime.serialPortPipe.on("data", (data)=> {
+                let previousReading = null, resetPreviousID = setInterval(()=> previousReading = null, 5000);
+                let readingInterval = setInterval(()=> $.post(runtime.server + "/read", {}, (data)=> {
                     let rfid = data.toString().trim();
-        
+                    if(previousReading == rfid)
+                        return;
+                    
+                    previousReading = rfid;
                     runtime.db.all("SELECT name, phone_number, ent_id, is_in FROM accounts WHERE rfid=\"" + rfid + "\"", (err, rows)=> {
-                        if(!err && rows.length >= 0) {
+                        if(!err && rows.length == 1) {
                             let date = new Date().toString();
 
                             date = date.substring(4, date.length);
@@ -250,7 +203,14 @@ const App = {
                             $("#log-table").prepend("<tr><td>" + date + "</td><td>" + rows[0].ent_id + "</td><td>" + rows[0].phone_number + "</td><td>" + rows[0].name + "</td></tr>");
                         }
                     });
-                });
+                }), 1000);
+
+                runtime.moveToSectionEvent = ()=> {
+                    dataTable.destroy();
+
+                    clearInterval(readingInterval);
+                    clearInterval(resetPreviousID);
+                };
             });
         });
     }
