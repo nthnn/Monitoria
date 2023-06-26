6f08979664bdd60ce9ec2cfef2915ecca2c5ed6b
nthnn <nathanneisip@gmail.com>
2023-06-26 17:04:13 +0800

    Showing log time on LCD I2C.

--- Monitoria-Attendance-ESP8266-System/src/main.cpp
+++ Monitoria-Attendance-ESP8266-System/src/main.cpp
@@ -97,6 +97,8 @@ void handleDataReceived() {
         lcd.print(server.arg("ent_name"));
         lcd.setCursor(0, 1);
         lcd.print(server.arg("ent_id"));
+        lcd.setCursor(10, 1);
+        lcd.print(server.arg("log_time"));
 
         server.send(200, "text/plain", "OK");
         sim800l.send_sms(server.arg("ent_cp"), MONITORIA_SMS_MESSAGE);
